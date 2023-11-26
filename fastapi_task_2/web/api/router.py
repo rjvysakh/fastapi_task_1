45d2fd10668fef769123813f8a5834b33cedf247
Vysakh R J <88417155+rjvysakh@users.noreply.github.com>
2023-11-27 01:24:50 +0530

    Update router.py

--- fastapi_task_2/web/api/router.py
+++ fastapi_task_2/web/api/router.py
@@ -1,9 +1,9 @@
 from fastapi.routing import APIRouter
 
-from fastapi_task_2.web.api import authentication, echo, monitoring
+from fastapi_task_2.web.api import authentication, document, monitoring
+
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(authentication.router, prefix="/auth", tags=["auth"])
-
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
+api_router.include_router(document.router, prefix="/document", tags=["document"])
